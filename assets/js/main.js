5ad5d2fd0771cd6581db273e37db7ccd128da20d
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2026-01-08 12:54:26 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,1 @@
-# Gehe ins richtige Verzeichnis
-cd /dein/projekt/verzeichnis
-
-# Lösche die fehlerhafte Datei und erstelle neu
-rm assets/js/main.js
-echo 'console.log("main.js geladen");' > assets/js/main.js
+console.log("main.js geladen");
