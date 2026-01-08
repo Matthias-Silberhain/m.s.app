7ed70dbb436b46406308cd94b9c72cb6ccceeb20
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2026-01-08 10:03:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = 'ms-app-v1';
+const urlsToCache = [
+  '/m.s.app/',
+  '/m.s.app/index.html',
+  '/m.s.app/manifest.json'
+];
 
+// Installieren
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Aktivieren
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
