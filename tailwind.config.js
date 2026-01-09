d41f5a2494d6ea3f2d9843ac7b5bf37af0b37109
MATTHIAS SILBERHAIN <m.silberhain@mail.de>
2026-01-09 19:12:06 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        silver: '#c0c0c0',
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+        'pulse-slow': 'pulse 3s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+}
